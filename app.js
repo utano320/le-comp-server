c9f1a220fdd186c60a1b51b9b1e10601b78c9fc3
Mitsuo Utano <utano320@gmail.com>
2018-11-23 10:08:46 +0900

    MySQLからデータ取得

--- app.js
+++ app.js
@@ -1,21 +1,36 @@
 import express from "express";
+import dotenv from "dotenv";
+import corser from "corser";
+import bodyParser from "body-parser";
+import mysql from "mysql";
+import dbConfig from "./config/database";
+import moment from "moment";
+import util from "util";
+
+dotenv.config();
+const con = mysql.createConnection(dbConfig);
+con.query = util.promisify(con.query);
+
+con.connect(err => {
+  if (err) throw err;
+
+  console.log("connected to mysql");
+});
 
 const app = express();
 
-app.get("/users", (req, res) => {
-  let sampleData = [
-    {
-      id: 1,
-      name: "うたの"
-    },
-    {
-      id: 2,
-      name: "su"
-    }
-  ];
+app.use(corser.create());
 
-  res.send(JSON.stringify(sampleData));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/users", async (req, res) => {
+  let rows = await con.query("select * from wd_x_sf");
+
+  res.send(JSON.stringify(rows));
 });
 
-// 8888番ポートで待機
-app.listen(8888, () => console.log("Listening on port 8888 ..."));
+// process.env.LISTEN_PORT番ポートで待機
+app.listen(process.env.LISTEN_PORT, () =>
+  console.log("Listening on port " + process.env.LISTEN_PORT + " ...")
+);
---
+++ config/database_default.js
@@ -0,0 +1,8 @@
+const dbConfig = {
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "le-comp"
+};
+
+module.exports = dbConfig;
